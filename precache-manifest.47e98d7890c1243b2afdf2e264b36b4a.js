68a764cc9eca0a5d629f2151ff46688e969bb29b
Rohit Kumar <rohitchaudhary701@gmail.com>
2019-12-02 11:09:54 +0530

    Updates

---
+++ precache-manifest.47e98d7890c1243b2afdf2e264b36b4a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ed2e6f64096787968863aa1148052da1",
+    "url": "/bluestacks/index.html"
+  },
+  {
+    "revision": "2c04b61d5255928cc253",
+    "url": "/bluestacks/static/css/2.b0bb7f2c.chunk.css"
+  },
+  {
+    "revision": "4f654ec8669d03f0a520",
+    "url": "/bluestacks/static/css/main.ccd18921.chunk.css"
+  },
+  {
+    "revision": "2c04b61d5255928cc253",
+    "url": "/bluestacks/static/js/2.30a2c3e4.chunk.js"
+  },
+  {
+    "revision": "4f654ec8669d03f0a520",
+    "url": "/bluestacks/static/js/main.584012d2.chunk.js"
+  },
+  {
+    "revision": "c3ef42deb18a26de5683",
+    "url": "/bluestacks/static/js/runtime-main.ae26ea32.js"
+  },
+  {
+    "revision": "66adda61d6f5a8f2f7bd70cd21117c7b",
+    "url": "/bluestacks/static/media/logo.66adda61.png"
+  }
+]);
